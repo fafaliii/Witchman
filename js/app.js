96324d8391a1838d3074610d79e0fd23aa4f1741
Александр Попович <popovicha154@mail.ru>
2023-02-26 22:14:52 +0300

    init: init

---
+++ js/app.js
@@ -0,0 +1,11 @@
+new Swiper('.slider',{
+    speed: 2600,
+    mousewheel: {
+        enabled: true,
+        sensitivity: 3
+
+    },
+    spaceBetween: 10,
+    parallax: true,
+    //freeMode: true,
+}) 
